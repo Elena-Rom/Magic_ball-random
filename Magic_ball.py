4b7c9f68158ec5a276dde899cf6c6a222da70f60
ElenaRomashkova <elen.iva89@gmail.com>
2017-06-09 14:58:49 +0300

    Код для игры, магический шар.

---
+++ Magic_ball.py
@@ -0,0 +1,9 @@
+import random
+print("Задайте интересующий вас вопрос, ответ на который будет односложным")
+ask=random.randint(1,3)
+if ask==1:
+ print("Да")
+elif ask==2:
+ print("Нет")
+elif ask==3:
+ print("Все в ваших руках")
